72a09113c55d0ab5edfa843b9f706c9060a86d78
Gourav Soni <gourav.soni@happiestminds.com>
2022-11-21 12:37:19 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { BookComponent } from './book/book.component';
+import { AppoComponent } from './appo/appo.component';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { RouterModule , Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes =[		
+  {path:'',redirectTo: 'home', pathMatch: 'full'},
+  {path:'home' , component:HomeComponent},
+  {path:'book' , component:BookComponent},
+  {path:'view' , component:AppoComponent},
+  {path:'contactUs' , component:ContactusComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactusComponent,
+    BookComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
